fe0a8895789b93b0a9c62c9698a528cb8fd6991b
kweku-xvi <nksarps@gmail.com>
2023-11-13 21:42:07 +0000

    books

---
+++ books/models.py
@@ -0,0 +1,27 @@
+import uuid
+from authors.models import Author
+from django.db import models
+from authors.models import Author
+
+
+class Book(models.Model):
+    BOOK_FORMAT = [
+        ('Hardcover', 'Hardcover'),
+        ('Paperback', 'Paperback'),
+        ('Ebook', 'Ebook'),
+        ('Other', 'Other')
+    ]
+
+    isbn = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.IntegerField(default=0)
+    format = models.CharField(max_length=255, choices=BOOK_FORMAT, default='Other')
+    edition = models.CharField(max_length=255)
+    date_published = models.DateField()
+    cover_image = models.ImageField(blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
