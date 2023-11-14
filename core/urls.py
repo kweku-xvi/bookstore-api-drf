80c5bd6ee49cf4027634fcc7ff4aae5533e8d8a7
kweku-xvi <nksarps@gmail.com>
2023-11-14 20:40:51 +0000

    added url endpoint for books app

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('authors/', include('authors.urls')),
+    path('books/', include('books.urls')),
 ]
