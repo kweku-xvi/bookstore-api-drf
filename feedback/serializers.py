828065c348bc40222d905487d3d43628fd9f6896
kweku-xvi <nksarps@gmail.com>
2023-11-16 21:06:16 +0000

    added user to the serializer view

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -3,6 +3,11 @@ from .models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    
     class Meta:
         model = Feedback
-        fields = ['id', 'category', 'title', 'text', 'created_at']
+        fields = ['id', 'category', 'title', 'text', 'created_at', 'user']
+
+    def get_user(self, obj):
+        return obj.user.username if obj.user else None
