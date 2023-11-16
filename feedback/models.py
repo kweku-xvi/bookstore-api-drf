39bcaae658b08590734f354e8ca6764e2aa1420e
kweku-xvi <nksarps@gmail.com>
2023-11-16 21:05:48 +0000

    added user subfield

--- feedback/models.py
+++ feedback/models.py
@@ -1,4 +1,5 @@
 import uuid
+from accounts.models import User
 from books.models import Book
 from django.db import models
 
@@ -17,6 +18,7 @@ class Feedback(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
