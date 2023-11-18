203390d60c192c8be9be456a844979fb4d8f9f90
kweku-xvi <nksarps@gmail.com>
2023-11-18 14:28:07 +0000

    added url endpoints for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('register/', views.create_user_view, name='register_user'),
     path('login/', views.login_user_view, name='login_user'),
-    path('users/', views.retrieve_all_users_view, name='retrieve_all_users')
+    path('users/', views.retrieve_all_users_view, name='retrieve_all_users'),
+    path('reset-password/', views.password_reset_view, name='reset_password_view'),
+    path('password-reset-confirm/', views.password_reset_view, name='password_reset_confirm_view'),
 ]
