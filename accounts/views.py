4e729cac9217445b34c5ca7e9ac1617ca2b0882e
kweku-xvi <nksarps@gmail.com>
2023-11-18 14:28:27 +0000

    added views for password reset

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,21 @@
+import os, jwt
 from .models import User
 from .serializers import RegisterUserSerializer, LoginUserSerializer, UsersSerializer
+from .utils import send_password_reset_email
+from dotenv import load_dotenv
+from django.urls import reverse
 from django.contrib.auth import authenticate
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes,permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
+
+load_dotenv()
 
 
 @api_view(['POST'])
@@ -80,5 +90,93 @@ def retrieve_all_users_view(request):
             }, status=status.HTTP_200_OK
         )
         
+@api_view(['POST'])
+def password_reset_view(request):
+    if request.method == "POST":
+        email = request.data.get('email')
+        if not email:
+            return Response(
+                {
+                    "success":False,
+                    "message":"Email is required",
+                },status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            user = User.objects.get(email=email)
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            current_site = get_current_site(request).domain
+            relative_link = reverse('password_reset_confirm_view')
+            absolute_url = f'http://{current_site}{relative_link}?uid={uid}&token={token}'
+            link = str(absolute_url)
+            send_password_reset_email(link=link,email=email, username=user.username)
+            return Response(
+                {
+                    "success":True,
+                    "message":"Password Reset Email Sent",
+                },status=status.HTTP_200_OK
+            )
+        except User.DoesNotExist as e:
+            return Response(
+                {
+                    "success":False,
+                    "message":"User Does Not Exist",
+                },status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "success":False,
+                    "message":str(e),
+                },status=status.HTTP_400_BAD_REQUEST
+            )
 
-# Todo : Email verification & Password Reset
+
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def reset_password_confirm_view(request):
+    if request.method == "PATCH":
+        uid = request.data.get("uid")
+        token = request.data.get("token")
+        password = request.data.get("password")
+        if not uid or not token or not password:
+            return Response(
+                {
+                    "success":False,
+                    "message":"All Fields Are Required",
+                },status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            user_id = urlsafe_base64_decode(uid)
+            user = User.objects.get(id=user_id)
+            if not default_token_generator.check_token(user,token):
+                return Response(
+                    {
+                        "success":False,
+                        "message":"Invalid Token",
+                    },status=status.HTTP_400_BAD_REQUEST
+                )
+            user.set_password(password)
+            user.save()
+            return Response(
+                {
+                    "success":True,
+                    "message":"Password Reset Successful",
+                },status=status.HTTP_200_OK
+            )
+        except User.DoesNotExist as e:
+            return Response(
+                {
+                    "success":False,
+                    "message":"User Does Not Exist",
+                },status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "success":False,
+                    "message":str(e),
+                },status=status.HTTP_400_BAD_REQUEST
+            )
+
+
