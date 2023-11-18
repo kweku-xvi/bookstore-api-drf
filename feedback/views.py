c9e155d17838b7195d36b6241ac079f3c1a6352d
kweku-xvi <nksarps@gmail.com>
2023-11-18 14:30:04 +0000

    added views for retrieving feedbacks with specified time periods

--- feedback/views.py
+++ feedback/views.py
@@ -1,26 +1,45 @@
 from .models import Feedback
 from .serializers import FeedbackSerializer
 from books.models import Book
+from datetime import timedelta
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def add_feedback_view(request, book_isbn):
+def get_book(book_isbn):
     try:
         book = Book.objects.get(isbn=book_isbn)
     except Book.DoesNotExist:
-        return Response (
+        return Response(
             {
                 'error':'This book does not exist'
             }, status=status.HTTP_404_NOT_FOUND
         )
+    return book
+
+
+def get_feedback(feedback_id):
+    try:
+        feedback = Feedback.objects.get(id=feedback_id)
+    except Feedback.DoesNotExist:
+        return Response(
+            {
+                'error':'This feedback does not exist'
+            }, status=status.HTTP_404_NOT_FOUND
+        )
+    return feedback
     
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_feedback_view(request, book_isbn): 
     if request.method == 'POST':
+        book = get_book(book_isbn)
+
         serializer = FeedbackSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -43,16 +62,9 @@ def add_feedback_view(request, book_isbn):
 @api_view(['GET'])
 @authentication_classes([])
 def get_feedback_view(request, feedback_id):
-    try:
-        feedback = Feedback.objects.get(id=feedback_id)
-    except Feedback.DoesNotExist:
-        return Response(
-            {
-                'error':'This feedback does not exist'
-            }, status=status.HTTP_404_NOT_FOUND
-        )
-
     if request.method == 'GET':
+        feedback = get_feedback(feedback_id)
+
         serializer = FeedbackSerializer(feedback)
 
         return Response(
@@ -65,16 +77,9 @@ def get_feedback_view(request, feedback_id):
 
 @api_view(['GET'])
 def get_all_feedbacks_on_a_book_view(request, book_isbn):
-    try:
-        book = Book.objects.get(isbn=book_isbn)
-    except Book.DoesNotExist:
-        return Response(
-            {
-                'error':'This book does not exist'
-            }, status=status.HTTP_404_NOT_FOUND
-        )
-
     if request.method == 'GET':
+        book = get_book(book_isbn)
+
         feedbacks = Feedback.objects.filter(book=book)
 
         serializer = FeedbackSerializer(feedbacks, many=True)
@@ -90,14 +95,7 @@ def get_all_feedbacks_on_a_book_view(request, book_isbn):
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def delete_feedback_view(request, feedback_id):
-    try:
-        feedback = Feedback.objects.get(id=feedback_id)
-    except Feedback.DoesNotExist:
-        return Response(
-            {
-                'error':'This feedback does not exist'
-            }, status=status.HTTP_404_NOT_FOUND
-        )
+    feedback = get_feedback(feedback_id)
 
     if request.method == 'DELETE':
         feedback.delete()
@@ -107,4 +105,85 @@ def delete_feedback_view(request, feedback_id):
                 'success':True,
                 'message':'The feedback has been deleted'
             }, status=status.HTTP_204_NO_CONTENT
+        )
+
+
+@api_view(['GET'])
+@authentication_classes([])
+def feedback_within_last_30_days(request, book_isbn):
+    time_frame = timezone.now() - timedelta(days=30)
+
+    if request.method == 'GET':
+        book = get_book(book_isbn)
+
+        feedbacks = Feedback.objects.filter(book=book, created_at__gte=time_frame)
+
+        serializer = FeedbackSerializer(feedbacks, many=True)
+
+        return Response(
+            {
+                'success':True,
+                'data':serializer.data
+            }, status=status.HTTP_200_OK
+        )
+
+
+@api_view(['GET'])
+@authentication_classes([])
+def feedback_within_last_3_months(request, book_isbn):
+    time_frame = timezone.now() - timedelta(days=90)
+
+    if request.method == 'GET':
+        book = get_book(book_isbn)
+
+        feedbacks = Feedback.objects.filter(book=book, created_at__gte=time_frame)
+
+        serializer = FeedbackSerializer(feedbacks, many=True)
+
+        return Response(
+            {
+                'success':True,
+                'data':serializer.data
+            }, status=status.HTTP_200_OK
+        )
+
+
+        
+@api_view(['GET'])
+@authentication_classes([])
+def feedback_within_last_6_months(request, book_isbn):
+    time_frame = timezone.now() - timedelta(days=180)
+
+    if request.method == 'GET':
+        book = get_book(book_isbn)
+
+        feedbacks = Feedback.objects.filter(book=book, created_at__gte=time_frame)
+
+        serializer = FeedbackSerializer(feedbacks, many=True)
+
+        return Response(
+            {
+                'success':True,
+                'data':serializer.data
+            }, status=status.HTTP_200_OK
+        )
+
+
+@api_view(['GET'])
+@authentication_classes([])
+def feedback_within_last_year(request, book_isbn):
+    time_frame = timezone.now() - timedelta(days=365)
+
+    if request.method == 'GET':
+        book = get_book(book_isbn)
+
+        feedbacks = Feedback.objects.filter(book=book, created_at__gte=time_frame)
+
+        serializer = FeedbackSerializer(feedbacks, many=True)
+
+        return Response(
+            {
+                'success':True,
+                'data':serializer.data
+            }, status=status.HTTP_200_OK
         )
