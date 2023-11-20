98fc61755318d9fab750c20d27ee0c8630115d09
kweku-xvi <nksarps@gmail.com>
2023-11-20 19:38:36 +0000

    refactor serializers

--- books/serializers.py
+++ books/serializers.py
@@ -3,11 +3,11 @@ from rest_framework import serializers
 
 
 class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['isbn', 'name', 'description', 'rating', 'format', 'edition', 'date_published', 'cover_image']
+    author = serializers.SerializerMethodField()
 
-class UpdateBookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['isbn', 'name', 'description', 'rating', 'format', 'edition', 'date_published', 'cover_image', 'author']
+    
+    def get_author(self, obj):
+        return obj.author.name if obj.author else None
