78860e0b56c93a8f4b99eaaad47f59e6a12d4484
kweku-xvi <nksarps@gmail.com>
2023-11-22 20:59:36 +0000

    added url endpoints for filtering and searching books

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('filter/', views.filter_books_by_genre_or_rating_view, name='filter_books_view'),
+    path('search/', views.search_books_view, name='search_books_view'),
     path('<uuid:author_id>/add/', views.add_book_view, name='add_book'),
     path('<uuid:author_id>/all-books/', views.retrieve_books_by_author, name='retrieve_books_by_author'),
     path('<uuid:book_isbn>/', views.retrieve_book_view, name='retrieve_specific_book'),
