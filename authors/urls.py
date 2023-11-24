dd2d9a317779bdada093550dc10aedd041512a7b
kweku-xvi <nksarps@gmail.com>
2023-11-24 18:47:27 +0000

    added url endpoint to retrieve all books by the author

--- authors/urls.py
+++ authors/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add/', views.add_author_view, name='add_author'),
     path('all/', views.retrieve_all_authors, name='retrieve_all_authors'),
     path('<uuid:author_id>/', views.retrieve_author_view, name='retrieve_particular_author'),
+    path('<uuid:author_id>/books/', views.retrieve_books_by_author, name='retrieve_books_by_author'),
     path('<uuid:author_id>/update/', views.update_author_view, name='update_author'),
     path('<uuid:author_id>/delete/', views.delete_author_view, name='delete_author'),
 ]
