ed0f138a47b99dd7d26861cd01af06c7e3c51b12
kweku-xvi <nksarps@gmail.com>
2023-11-25 16:38:24 +0000

    updated allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'djmoney',
     'feedback',
     'rest_framework',
+    'payments',
 ]
 
 MIDDLEWARE = [
