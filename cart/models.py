46fa9c5acc7c47d470b583e4e5d96ec7cea41c9b
kweku-xvi <nksarps@gmail.com>
2023-11-26 17:16:16 +0000

    added model for cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
+from books.models import Book
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}'s Cart"
+
+
+class CartItem(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+
+    def __str__(self):
+        return f"{self.quantity} x {self.book.name}"
