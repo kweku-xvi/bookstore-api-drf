a7648b5eaadc644bfacf02b1226f35edac6745e8
kweku-xvi <nksarps@gmail.com>
2023-11-26 17:16:44 +0000

    added cartitemserializer

---
+++ cart/serializers.py
@@ -0,0 +1,11 @@
+from .models import CartItem
+from books.serializers import BookSerializer
+from rest_framework import serializers
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = ['book', 'quantity']
