16278a5af69d18495369f7f8c5779b0ba33e923a
kweku-xvi <nksarps@gmail.com>
2023-11-26 17:21:40 +0000

    refactor serializers

--- books/serializers.py
+++ books/serializers.py
@@ -7,7 +7,7 @@ class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
-        fields = ['isbn', 'name', 'cover_image','description', 'edition', 'format', 'date_published', 'genre', 'rating', 'author','price', 'language', 'quantity']
+        fields = ['isbn', 'name', 'cover_image','description', 'edition', 'format', 'date_published', 'genre', 'rating', 'author','price', 'language', 'remaining_books']
     
     def get_author(self, obj):
         return obj.author.name if obj.author else None
