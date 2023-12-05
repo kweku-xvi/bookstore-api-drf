ff250bac783ed0633d7900012476ae9ee3bf8c6c
kweku-xvi <nksarps@gmail.com>
2023-12-05 16:36:59 +0000

    refactor urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('register/', views.create_user_view, name='register_user'),
-    path('login/', views.login_user_view, name='login_user'),
-    path('users/', views.retrieve_all_users_view, name='retrieve_all_users'),
-    path('reset-password/', views.password_reset_view, name='reset_password_view'),
-    path('password-reset-confirm/', views.password_reset_view, name='password_reset_confirm_view'),
+    path('register', views.create_user_view, name='register_user'),
+    path('verify-user', views.verify_user_view, name='verify_user'),
+    path('login', views.login_user_view, name='login_user'),
+    path('users', views.retrieve_all_users_view, name='retrieve_users'),
+    path('password-reset', views.password_reset_view, name='password_reset'),
+    path('password-reset-confirm', views.password_reset_confirm_view, name='password_reset_confirm'),
 ]
