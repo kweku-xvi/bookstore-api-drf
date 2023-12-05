da9bcfcd8adf5c7314346b77e31f2f7c95e9f745
kweku-xvi <nksarps@gmail.com>
2023-12-05 16:40:02 +0000

    refactor urls

--- books/urls.py
+++ books/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('filter/', views.filter_books_by_genre_or_rating_view, name='filter_books_view'),
-    path('search/', views.search_books_view, name='search_books_view'),
-    path('<uuid:author_id>/add/', views.add_book_view, name='add_book'),
-    path('<uuid:book_isbn>/', views.retrieve_book_view, name='retrieve_specific_book'),
-    path('<uuid:book_isbn>/update/', views.update_book_details_view, name='update_book_details'),
-    path('<uuid:book_isbn>/delete/', views.delete_book_view, name='delete_book'),
+    path('filter', views.filter_books_by_genre_or_rating_view, name='filter_books_view'),
+    path('search', views.search_books_view, name='search_books_view'),
+    path('<uuid:author_id>/add', views.add_book_view, name='add_book'),
+    path('<str:book_isbn>', views.retrieve_book_view, name='retrieve_specific_book'),
+    path('<str:book_isbn>/update', views.update_book_details_view, name='update_book_details'),
+    path('<str:book_isbn>/delete', views.delete_book_view, name='delete_book'),
 ]
