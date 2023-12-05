b296678f4491aaa73e38116336abfe9fc08b7d61
kweku-xvi <nksarps@gmail.com>
2023-12-05 16:36:42 +0000

    refactor utils

--- accounts/utils.py
+++ accounts/utils.py
@@ -6,15 +6,29 @@ load_dotenv()
 
 client = Courier(auth_token=os.getenv('AUTH_TOKEN'))
 
+def send_verification_email(email, link, username):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": "HQRFKDHDK84B16GJAQ7PWPFATXS8",
+            "data": {
+            "username": username,
+            "link": link,
+            },
+        }
+    )
+
 def send_password_reset_email(username, email, link):
     client.send_message(
         message={
             "to": {
             "email": email,
             },
-            "template": "CKDTAH7B7JM7HKHJFM2NQJ8YFZR6",
+            "template": "WF7909Y7ZWMNWNNTNNQRHDTBKDF4",
             "data": {
-            "user": username,
+            "username": username,
             "link": link,
             },
         }
