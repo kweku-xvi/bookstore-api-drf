35e51a8c2e1ac3a315ad85cabb07905444ac56ae
kweku-xvi <nksarps@gmail.com>
2023-12-06 18:24:49 +0000

    refactor urls

--- payments/urls.py
+++ payments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.initialize_payments, name='initialize_payments'),
+    path('<uuid:order_id>', views.checkout, name='initialize_payments'),
     path('webhook', views.payment_webhook, name='payment_webhook')
 ]
