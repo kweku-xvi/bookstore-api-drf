fb6b0d43ee37632817bdd75b6002bc5fb9413015
kweku-xvi <nksarps@gmail.com>
2023-12-06 18:17:15 +0000

    update quantity attribute

--- cart/models.py
+++ cart/models.py
@@ -14,20 +14,9 @@ class Cart(models.Model):
 class CartItem(models.Model):
     id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=1)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
 
     def __str__(self):
         return f"{self.book.name} - Quantity {self.quantity}"
 
-
-class OrderDetails(models.Model):
-    order_id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    items = models.ManyToManyField(CartItem, related_name='cart_items')
-    total_amount = models.DecimalField(max_digits=14, decimal_places=2, default=0.00)
-    order_date = models.DateTimeField(auto_now_add=True)
-    is_completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Order #{self.order_id} - {self.user.username} - Total: {self.total_amount}"
