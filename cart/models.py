7121678e4de941f51f245edc12f0bec0b0abe85e
kweku-xvi <nksarps@gmail.com>
2023-12-06 13:16:03 +0000

    add orderdetails model

--- cart/models.py
+++ cart/models.py
@@ -19,3 +19,15 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.book.name} - Quantity {self.quantity}"
+
+
+class OrderDetails(models.Model):
+    order_id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem, related_name='cart_items')
+    total_amount = models.DecimalField(max_digits=14, decimal_places=2, default=0.00)
+    order_date = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order #{self.order_id} - {self.user.username} - Total: {self.total_amount}"
