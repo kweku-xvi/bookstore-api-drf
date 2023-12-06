7d62469c96301cc5d3571b28db12ec438c97e103
kweku-xvi <nksarps@gmail.com>
2023-12-06 18:25:44 +0000

    update views

--- payments/views.py
+++ payments/views.py
@@ -3,24 +3,47 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
-from .utils import initialize_transactions
-import json
+from .utils import initialize_transactions, verify_payment,  generate_payment_id
+from .models import Payment
+import json, random, string
+from orders.models import Order
+from accounts.permissions import IsVerified
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def initialize_payments(request):
-    user = request.user
-    email = user.email
+@permission_classes([IsVerified])
+def checkout(request, order_id):
+    if request.method == 'POST':
+        user = request.user 
+        email = user.email 
 
-    transaction = initialize_transactions(email=email, amount='100', order_id='kyyya74ajdo25if8ag')
+        try:
+            order = Order.objects.get(order_id=order_id)
 
-    return Response (
-        {
-            'success':True,
-            'transaction_url':transaction,
-        }
-    )
+            transaction = initialize_transactions(email=email, amount=str(100 * order.total_amount), payment_id=generate_payment_id())
 
+            payment_data = verify_payment(transaction)
+            if payment_data:
+                payment = Payment.objects.create(
+                    user=user,
+                    order=order,
+                    amount=100 * order.total_amount,
+                )
+
+            return Response(
+                    {
+                        'success':True,
+                        'transaction_url':transaction
+                    }, status=status.HTTP_200_OK
+                )
+        except Order.DoesNotExist:
+            return Response(
+                {
+                    'success':False,
+                    'message':'The order does not exist'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        
 
 @api_view(['POST'])
 @authentication_classes([])
@@ -39,3 +62,5 @@ def payment_webhook(request):
             'reference_id':reference,
         }, status=status.HTTP_200_OK
     )
+
+
