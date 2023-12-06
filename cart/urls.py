2632d3f04b60c2bcaf4eed26293c5e0b2420eac1
kweku-xvi <nksarps@gmail.com>
2023-12-06 18:17:35 +0000

    refactor urls

--- cart/urls.py
+++ cart/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.get_all_books_in_the_cart, name='cart_items'),
     path('<uuid:cart_id>', views.get_cart_item, name='get_cart_item'),
-    path('<uuid:cart_id>/order', views.order_particular_cart_item, name='order_particular_cart_item'),
     path('<str:book_isbn>/add', views.add_book_to_cart, name='add_book_to_cart'),
     path('<uuid:cart_id>/remove', views.remove_cart_item, name='remove_cart_item'),
-    path('order/all', views.order_all_items_in_cart, name='order_all_items_in_cart')
 ]
