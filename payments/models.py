1a9c1b4f9bddd3fb6b955337018fc6eae4606d1a
kweku-xvi <nksarps@gmail.com>
2023-12-06 18:24:25 +0000

    update payment model

--- payments/models.py
+++ payments/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 import uuid
 from accounts.models import User
+from orders.models import Order
+from django.utils import timezone
 
-
-class Payments(models.Model):
+class Payment(models.Model):
     id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
-    amount = models.DecimalField(max_digits=14, decimal_places=2, default=0.00)
+    amount = models.DecimalField(max_digits=14, decimal_places=2, default=0.00, validators=[MinValueValidator(limit_value=0)])
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_id = models.UUIDField(unique=True, default=uuid.uuid4)
-
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    paid_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.id
+        return F'Payment #{self.id} - {self.user.username} - Amount: {self.amount}'
+
+    class Meta:
+        ordering = ('-paid_at', )
