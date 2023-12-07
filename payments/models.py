b76485a655d0ecbf07c11773876c3bbd1e186b6b
kweku-xvi <nksarps@gmail.com>
2023-12-07 12:57:52 +0000

    update payment model

--- payments/models.py
+++ payments/models.py
@@ -9,7 +9,7 @@ class Payment(models.Model):
     id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
     amount = models.DecimalField(max_digits=14, decimal_places=2, default=0.00, validators=[MinValueValidator(limit_value=0)])
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, blank=True, null=True)
     paid_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
