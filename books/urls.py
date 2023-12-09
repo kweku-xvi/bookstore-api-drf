b83da4474012c3264ab531955f683f6208050579
kweku-xvi <nksarps@gmail.com>
2023-12-09 15:23:19 +0000

    add url to retrieve all books

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.get_all_books_view, name='get_all_books'),
     path('filter', views.filter_books_by_genre_or_rating_view, name='filter_books_view'),
     path('search', views.search_books_view, name='search_books_view'),
     path('<uuid:author_id>/add', views.add_book_view, name='add_book'),
